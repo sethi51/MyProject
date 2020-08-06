649f2a46af6b21af951474f97f59d856ba4f73be
sethi <sethi@localhost>
2020-08-06 11:42:59 +0530

    Update1

--- EmployeeManagmentSystem/src/com/lti/VideoGame.java
+++ EmployeeManagmentSystem/src/com/lti/VideoGame.java
@@ -3,7 +3,7 @@ package com.lti;
 public class VideoGame {
 	
 	public void pubg(){
-		
+		System.out.println("PubG is working");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
