c8687c8b86216f9e72f7d63c1ce07b122dac9bbe
sethi <sethi@localhost>
2020-08-06 11:45:06 +0530

    Hello1 implemented

--- EmployeeManagmentSystem/src/com/lti/VideoGame.java
+++ EmployeeManagmentSystem/src/com/lti/VideoGame.java
@@ -9,7 +9,7 @@ public class VideoGame {
 		
 	}
 	public void hello1(int a){
-		
+		System.out.println("Change");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
