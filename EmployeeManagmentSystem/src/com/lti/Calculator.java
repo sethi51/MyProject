d160e924df8289a8b6968b7b2ee223141f695ad0
sethi <sethi@localhost>
2020-08-06 09:47:19 +0530

    Add Diff Method

--- EmployeeManagmentSystem/src/com/lti/Calculator.java
+++ EmployeeManagmentSystem/src/com/lti/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
 	public static void add(int a,int b) {
 		System.out.println("Sum = "+(a+b));
 	}
+	public static void diff(int a,int b) {
+		System.out.println("Difference = "+(a-b));
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		add(10,30);
